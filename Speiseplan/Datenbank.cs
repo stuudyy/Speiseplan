b85e73b29baafdf015544a3c59156b46650f95a0
Florian07 <florian.haberle@s.hakmistelbach.ac.at>
2021-06-06 19:20:06 +0200

    Klasse

---
+++ Speiseplan/Datenbank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace Speiseplan
+{
+    class Datenbank
+    {
+        private OleDbConnection verbindung;
+        private OleDbCommand cmd;
+        private string cn;
+
+
+        public Datenbank()
+        {
+            cn = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=Speiseplan.accdb";
+            verbindung = new OleDbConnection(cn);
+            
+        }
+
+        public OleDbDataReader Einlesen(string sql)
+        {
+            try
+            {
+                verbindung.Close();
+                verbindung.Open();
+                cmd = new OleDbCommand(sql, verbindung);
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fehler beim Einlesen: " + ex.Message);
+            }
+        }
+
+        public void Ausfuehren(string sql)
+        {
+            try
+            {
+                verbindung.Close();
+                verbindung.Open();
+                cmd = new OleDbCommand(sql, verbindung);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fehler beim Ausführen: " + ex.Message);
+            }
+            finally
+            {
+                verbindung.Close();
+            }
+        }
+    }
+}
--- Speiseplan/Form1.Designer.cs
+++ Speiseplan/Form1.Designer.cs
@@ -204,7 +204,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1295, 710);
+            this.ClientSize = new System.Drawing.Size(1295, 750);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
