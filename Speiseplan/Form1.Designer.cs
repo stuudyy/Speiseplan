4b34d474512277ed265b6cd5b0fc66e22b6ba6c7
Florian07 <florian.haberle@s.hakmistelbach.ac.at>
2021-06-06 18:56:41 +0200

    Form1 bearbeitet

--- Speiseplan/Form1.Designer.cs
+++ Speiseplan/Form1.Designer.cs
@@ -30,43 +30,32 @@
         {
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnMontag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnDienstag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnMittwoch = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnDonnerstag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnFreitag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listView2 = new System.Windows.Forms.ListView();
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listView3 = new System.Windows.Forms.ListView();
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader13 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.columnVorspeise = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnIDVorspeise = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
-            this.columnMontag,
-            this.columnDienstag,
-            this.columnMittwoch,
-            this.columnDonnerstag,
-            this.columnFreitag});
+            this.columnIDVorspeise,
+            this.columnVorspeise});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 54);
+            this.listView1.Location = new System.Drawing.Point(16, 66);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(504, 425);
+            this.listView1.Size = new System.Drawing.Size(358, 522);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -75,44 +64,17 @@
             // 
             this.columnHeader1.Width = 0;
             // 
-            // columnMontag
-            // 
-            this.columnMontag.Text = "Montag";
-            this.columnMontag.Width = 100;
-            // 
-            // columnDienstag
-            // 
-            this.columnDienstag.Text = "Dienstag";
-            this.columnDienstag.Width = 100;
-            // 
-            // columnMittwoch
-            // 
-            this.columnMittwoch.Text = "Mittwoch";
-            this.columnMittwoch.Width = 100;
-            // 
-            // columnDonnerstag
-            // 
-            this.columnDonnerstag.Text = "Donnerstag";
-            this.columnDonnerstag.Width = 100;
-            // 
-            // columnFreitag
-            // 
-            this.columnFreitag.Text = "Freitag";
-            this.columnFreitag.Width = 100;
-            // 
             // listView2
             // 
             this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2,
             this.columnHeader3,
-            this.columnHeader4,
-            this.columnHeader5,
-            this.columnHeader6,
-            this.columnHeader7});
+            this.columnHeader4});
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(559, 54);
+            this.listView2.Location = new System.Drawing.Point(410, 66);
+            this.listView2.Margin = new System.Windows.Forms.Padding(4);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(504, 425);
+            this.listView2.Size = new System.Drawing.Size(367, 522);
             this.listView2.TabIndex = 1;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -123,42 +85,24 @@
             // 
             // columnHeader3
             // 
-            this.columnHeader3.Text = "Montag";
-            this.columnHeader3.Width = 100;
+            this.columnHeader3.Text = "ID";
             // 
             // columnHeader4
             // 
-            this.columnHeader4.Text = "Dienstag";
-            this.columnHeader4.Width = 100;
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Mittwoch";
-            this.columnHeader5.Width = 100;
-            // 
-            // columnHeader6
-            // 
-            this.columnHeader6.Text = "Donnerstag";
-            this.columnHeader6.Width = 100;
-            // 
-            // columnHeader7
-            // 
-            this.columnHeader7.Text = "Freitag";
-            this.columnHeader7.Width = 100;
+            this.columnHeader4.Text = "Hauptspeise";
+            this.columnHeader4.Width = 200;
             // 
             // listView3
             // 
             this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader8,
             this.columnHeader9,
-            this.columnHeader10,
-            this.columnHeader11,
-            this.columnHeader12,
-            this.columnHeader13});
+            this.columnHeader10});
             this.listView3.HideSelection = false;
-            this.listView3.Location = new System.Drawing.Point(1104, 54);
+            this.listView3.Location = new System.Drawing.Point(815, 66);
+            this.listView3.Margin = new System.Windows.Forms.Padding(4);
             this.listView3.Name = "listView3";
-            this.listView3.Size = new System.Drawing.Size(504, 425);
+            this.listView3.Size = new System.Drawing.Size(364, 522);
             this.listView3.TabIndex = 2;
             this.listView3.UseCompatibleStateImageBehavior = false;
             this.listView3.View = System.Windows.Forms.View.Details;
@@ -169,36 +113,21 @@
             // 
             // columnHeader9
             // 
-            this.columnHeader9.Text = "Montag";
-            this.columnHeader9.Width = 100;
+            this.columnHeader9.Text = "ID";
             // 
             // columnHeader10
             // 
-            this.columnHeader10.Text = "Dienstag";
-            this.columnHeader10.Width = 100;
-            // 
-            // columnHeader11
-            // 
-            this.columnHeader11.Text = "Mittwoch";
-            this.columnHeader11.Width = 100;
-            // 
-            // columnHeader12
-            // 
-            this.columnHeader12.Text = "Donnerstag";
-            this.columnHeader12.Width = 100;
-            // 
-            // columnHeader13
-            // 
-            this.columnHeader13.Text = "Freitag";
-            this.columnHeader13.Width = 100;
+            this.columnHeader10.Text = "Nachspeise";
+            this.columnHeader10.Width = 200;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(207, 20);
+            this.label1.Location = new System.Drawing.Point(125, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.Size = new System.Drawing.Size(108, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Vorspeise";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -207,9 +136,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(762, 20);
+            this.label2.Location = new System.Drawing.Point(526, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 18);
+            this.label2.Size = new System.Drawing.Size(134, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Hauptspeise";
             // 
@@ -217,25 +147,37 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1282, 20);
+            this.label3.Location = new System.Drawing.Point(930, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 18);
+            this.label3.Size = new System.Drawing.Size(124, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Nachspeise";
             // 
+            // columnVorspeise
+            // 
+            this.columnVorspeise.Text = "Vorspeise";
+            this.columnVorspeise.Width = 200;
+            // 
+            // columnIDVorspeise
+            // 
+            this.columnIDVorspeise.Text = "ID";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1620, 577);
+            this.ClientSize = new System.Drawing.Size(1295, 710);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView3);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,28 +187,19 @@
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnMontag;
-        private System.Windows.Forms.ColumnHeader columnDienstag;
-        private System.Windows.Forms.ColumnHeader columnMittwoch;
-        private System.Windows.Forms.ColumnHeader columnDonnerstag;
-        private System.Windows.Forms.ColumnHeader columnFreitag;
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
-        private System.Windows.Forms.ColumnHeader columnHeader5;
-        private System.Windows.Forms.ColumnHeader columnHeader6;
-        private System.Windows.Forms.ColumnHeader columnHeader7;
         private System.Windows.Forms.ListView listView3;
         private System.Windows.Forms.ColumnHeader columnHeader8;
         private System.Windows.Forms.ColumnHeader columnHeader9;
         private System.Windows.Forms.ColumnHeader columnHeader10;
-        private System.Windows.Forms.ColumnHeader columnHeader11;
-        private System.Windows.Forms.ColumnHeader columnHeader12;
-        private System.Windows.Forms.ColumnHeader columnHeader13;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader columnIDVorspeise;
+        private System.Windows.Forms.ColumnHeader columnVorspeise;
     }
 }
 
